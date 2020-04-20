18ff4d2eea61798f5be566fee77bb2061a2c84c2
Dricosta <adrianoc55@hotmail.com>
2020-04-20 17:50:14 -0300

    two tests passed 2/3

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,13 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+
+interface Request {
+  title: string,
+  value: number,
+  type: 'income' | 'outcome'
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +15,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+
+
+    const balance = this.transactionsRepository.getBalance();
+
+    switch (type) {
+      case "outcome":
+        if(value > balance.total){
+          throw Error('Você não tem saldo suficiente');
+        }
+        break;
+      default:
+        break;
+    }
+
+    const transactions = this.transactionsRepository.create({ title, value, type });
+
+    return transactions;
   }
 }
 
